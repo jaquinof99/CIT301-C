da86061be359beabffa7646575578fecd7b9c048
Jordan <jaquinof@pucp.pe>
2017-02-23 08:36:23 -0700

    Lesson 4, DoNe!!!! ...error debugged....Final

--- ContactApp/src/app/messages/message-list/message-list.component.ts
+++ ContactApp/src/app/messages/message-list/message-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import {MessagesService} from "../messages.service";
+import {Message} from "../message";
 @Component({
   selector: 'ca-message-list',
   templateUrl: './message-list.component.html',
   styleUrls: ['./message-list.component.css']
 })
 export class MessageListComponent implements OnInit {
+  messages: Message[] = [];
+  constructor(private ms: MessagesService ) {
 
-  constructor() { }
+  }
 
   ngOnInit() {
+    this.messages=this.ms.getMessages();
   }
+
 
 }
