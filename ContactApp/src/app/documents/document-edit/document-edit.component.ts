9a54feacee56ab55d36f6954cab4f1355dd4848e
Jordan <jaquinof@pucp.pe>
2017-03-16 11:58:46 -0600

    lesson 6, page 9

--- ContactApp/src/app/documents/document-edit/document-edit.component.ts
+++ ContactApp/src/app/documents/document-edit/document-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'ca-document-edit',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-edit.component.css']
 })
 export class DocumentEditComponent implements OnInit {
-
+  @Input() oldDoc: Document;
   constructor() { }
 
   ngOnInit() {
--- ContactApp/src/app/documents/document-routes.ts
+++ ContactApp/src/app/documents/document-routes.ts
@@ -5,7 +5,7 @@ import {DocumentEditComponent} from "./document-edit/document-edit.component";
 
 export const DOCUMENT_ROUTES: Routes = [
 
+  {path: 'new', component: DocumentEditComponent},
   {path: ':idx', component: DocumentsViewComponent},
-  {path:':idx/edit', component: DocumentEditComponent},
-  {path: 'new', component: DocumentEditComponent}
+  {path:':idx/edit', component: DocumentEditComponent}
 ]
