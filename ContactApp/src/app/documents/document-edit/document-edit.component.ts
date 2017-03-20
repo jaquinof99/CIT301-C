fb5b8a95667fab7683fb77e3fb1ccaa40a8201a5
Jordan <jaquinof@pucp.pe>
2017-03-19 20:31:21 -0600

    lesson 6, page 13

--- ContactApp/src/app/documents/document-edit/document-edit.component.ts
+++ ContactApp/src/app/documents/document-edit/document-edit.component.ts
@@ -1,15 +1,62 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, Input, OnDestroy} from '@angular/core';
+import {Subscription} from "rxjs";
+import {ActivatedRoute, Router} from "@angular/router";
+import {DocumentsService} from "../documents.service";
+import {Document} from "../document";
 
 @Component({
   selector: 'ca-document-edit',
   templateUrl: './document-edit.component.html',
   styleUrls: ['./document-edit.component.css']
 })
-export class DocumentEditComponent implements OnInit {
-  @Input() oldDoc: Document;
-  constructor() { }
+export class DocumentEditComponent implements OnInit,OnDestroy {
+
+  private subscription: Subscription;
+  private oldDocument: Document;
+  private editMode: boolean = false;
+  private documentIdx: number;
+
+  constructor(private route: ActivatedRoute,
+              private documentsService: DocumentsService,
+              private router: Router
+  ) { }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+      (params: any)=>{
+        if(params.hasOwnProperty('idx')){
+          this.editMode=true;
+          this.documentIdx = +params['idx'];
+          this.oldDocument = this.documentsService.getDocument(this.documentIdx);
+        }else{
+          this.editMode = false;
+          this.oldDocument = null;
+        }
+        //this.initForm();
+      }
+    );
   }
 
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+
+  onSubmit(value){
+    let newDocument = new Document(null,
+                                    value.documentTitle,
+                                    value.documentDescription,
+                                    value.documentURL, null);
+    console.log(newDocument);
+    if (this.editMode){
+      newDocument.id = this.oldDocument.id;
+      this.documentsService.updateDocument(this.oldDocument, newDocument);
+    }else{
+      this.documentsService.addDocument(newDocument);
+    }
+    this.router.navigate(['documents']);
+  }
+
+  onCancel(){
+    this.router.navigate(['../']);
+  }
 }
